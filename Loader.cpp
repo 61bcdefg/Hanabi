21cddaf41867cfe628c3b1c9ec2701054aa57c37
abcdefg <78954867+61bcdefg@users.noreply.github.com>
2023-03-03 21:51:00 +0800

    fix building, refactor

--- Loader.cpp
+++ Loader.cpp
@@ -1,19 +1,24 @@
-// For open-source license, please refer to [License](https://github.com/HikariObfuscator/Hikari/wiki/License).
+// For open-source license, please refer to
+// [License](https://github.com/HikariObfuscator/Hikari/wiki/License).
 #include "dobby.h"
-#include <llvm/Transforms/Obfuscation/Obfuscation.h>
-#include <llvm/IR/PassManager.h>
 #include <llvm/Config/abi-breaking.h>
-#include <string>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/Transforms/Obfuscation/Obfuscation.h>
 #include <mach-o/dyld.h>
+#include <string>
 #include <sys/sysctl.h>
+
 #if LLVM_ENABLE_ABI_BREAKING_CHECKS
 #error "Configure LLVM with -DLLVM_ABI_BREAKING_CHECKS=FORCE_OFF"
 #endif
-using namespace std;
-void (*old_pmb)(void* dis,legacy::PassManagerBase &MPM);
-static void new_pmb(void* dis,legacy::PassManagerBase &MPM){
+
+using namespace llvm;
+
+void (*old_pmb)(void *dis, legacy::PassManagerBase &MPM);
+static void new_pmb(void *dis, legacy::PassManagerBase &MPM) {
   MPM.add(createObfuscationLegacyPass());
-  old_pmb(dis,MPM);
+  old_pmb(dis, MPM);
 }
 
 static ModulePassManager buildObfuscationPipeline(void) {
@@ -27,9 +32,9 @@ static Error new_pmp(ModulePassManager &MPM, int E) {
   return old_pmp(MPM, E);
 }
 
-static __attribute__((__constructor__)) void Inj3c73d(int argc, char* argv[]){
-  char* executablePath=argv[0];
-  //Initialize our own LLVM Library
+static __attribute__((__constructor__)) void Inj3c73d(int argc, char *argv[]) {
+  char *executablePath = argv[0];
+  // Initialize our own LLVM Library
   if (strstr(executablePath, "swift-frontend"))
     errs() << "Applying Apple SwiftC Hooks...\n";
   else
@@ -37,9 +42,21 @@ static __attribute__((__constructor__)) void Inj3c73d(int argc, char* argv[]){
 #if defined(__x86_64__)
   int ret = 0;
   size_t size = sizeof(ret);
-  if (sysctlbyname("sysctl.proc_translated", &ret, &size, NULL, 0) != -1 && ret == 1)
-    errs() << "[Hanabi] Looks like you are currently running the process in Rosetta 2 mode, which will prevent DobbyHook from working.\nPlease close it.\n";
+  if (sysctlbyname("sysctl.proc_translated", &ret, &size, NULL, 0) != -1 &&
+      ret == 1)
+    errs() << "[Hanabi] Looks like you are currently running the process in "
+              "Rosetta 2 mode, which will prevent DobbyHook from "
+              "working.\nPlease close it.\n";
 #endif
-  DobbyHook(DobbySymbolResolver(executablePath, "__ZN4llvm18PassManagerBuilder25populateModulePassManagerERNS_6legacy15PassManagerBaseE"), (dobby_dummy_func_t)new_pmb, (dobby_dummy_func_t *)&old_pmb);
-  DobbyHook(DobbySymbolResolver(executablePath, "__ZN4llvm11PassBuilder15parseModulePassERNS_11PassManagerINS_6ModuleENS_15AnalysisManagerIS2_JEEEJEEERKNS0_15PipelineElementE"), (dobby_dummy_func_t)new_pmp, (dobby_dummy_func_t *)&old_pmp);
+  DobbyHook(DobbySymbolResolver(
+                executablePath,
+                "__ZN4llvm18PassManagerBuilder25populateModulePassManagerERNS_"
+                "6legacy15PassManagerBaseE"),
+            (dobby_dummy_func_t)new_pmb, (dobby_dummy_func_t *)&old_pmb);
+  DobbyHook(
+      DobbySymbolResolver(
+          executablePath,
+          "__ZN4llvm11PassBuilder15parseModulePassERNS_11PassManagerINS_"
+          "6ModuleENS_15AnalysisManagerIS2_JEEEJEEERKNS0_15PipelineElementE"),
+      (dobby_dummy_func_t)new_pmp, (dobby_dummy_func_t *)&old_pmp);
 }
